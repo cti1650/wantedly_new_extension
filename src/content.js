cfb97ca05db72dea4620caf92297f740678746f5
cti1650 <cti1650@gmail.com>
2021-08-07 23:01:20 +0900

    🔨 URLを切り替える機能を実装

---
+++ src/content.js
@@ -0,0 +1,12 @@
+(function (d) {
+  let url = window.location.href;
+  console.log(url);
+  if (~url.indexOf('/projects')) {
+    if (~url.indexOf('type=mixed')) {
+      url = url.replace('type=mixed', 'type=recent');
+      window.location.href = url;
+    } else if (url === 'https://www.wantedly.com/projects') {
+      window.location.href = url + '?type=recent';
+    }
+  }
+})(document);
