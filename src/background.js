bbf738b8b260ec3fc58d4297580e8bc1be751a5d
cti1650 <cti1650@gmail.com>
2021-08-09 21:58:59 +0900

    🔨 リファクタリング

--- src/background.js
+++ src/background.js
@@ -1,11 +1,11 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (!changeInfo.status) return;
   let url = tab.url;
-  if (~url.indexOf('https://www.wantedly.com/projects')) {
-    if (~url.indexOf('type=mixed')) {
-      url = url.replace('type=mixed', 'type=recent');
-      chrome.tabs.update(tabId, { url: url });
-    } else if (url === 'https://www.wantedly.com/projects') {
-      chrome.tabs.update(tabId, { url: url + '?type=recent' });
-    }
+  if (!~url.indexOf('https://www.wantedly.com/projects')) return;
+  if (~url.indexOf('type=mixed')) {
+    url = url.replace('type=mixed', 'type=recent');
+    chrome.tabs.update(tabId, { url: url });
+  } else if (url === 'https://www.wantedly.com/projects') {
+    chrome.tabs.update(tabId, { url: url + '?type=recent' });
   }
 });
